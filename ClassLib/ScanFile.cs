40529b702115b261c5c97f6e6bf977f76823a630
Yung, Chun Kau <yung.chun.kau@gmail.com>
2023-02-02 13:39:58 +0800

    v0.0.0.1

---
+++ ClassLib/ScanFile.cs
@@ -0,0 +1,9 @@
+﻿namespace Dir;
+
+public static class Scan
+{
+    static public IEnumerable<string> ListFile(string path)
+    {
+        return Directory.GetFiles(path);
+    }
+}
---
+++ DemoCs/Program.cs
@@ -0,0 +1,8 @@
+﻿using Dir;
+
+var dirThe = ".";
+Console.WriteLine($"This is {Path.GetFullPath(dirThe)}");
+foreach (var nameThe in Scan.ListFile(dirThe))
+{
+    Console.WriteLine(nameThe);
+}
